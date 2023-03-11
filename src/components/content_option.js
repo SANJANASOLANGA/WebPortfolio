6ed82c65e72ea2b1daaf4d845c23ecfaf2330b9f
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-11 21:07:56 +0530

    Added initial content_option.js

---
+++ src/components/content_option.js
@@ -0,0 +1,20 @@
+const meta = {
+    title: "Sewmini Sanjana",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const contactConfig = {
+    YOUR_EMAIL: "sanjanasolangaarachchi@gmail.com",
+    YOUR_FONE: "+94719640127",
+    description: "If you have any issue, feel free to contact me ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+export {
+    meta,
+    contactConfig,
+};

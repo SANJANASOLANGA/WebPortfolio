5e179469b75d2f85e486eab2b37d3ed6a6012c1c
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-11 21:11:21 +0530

    Added HelmetProvider

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,6 +5,8 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Sewmini_Sanjana_Web.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../content_option";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -14,30 +16,37 @@ function ResumeNew() {
   }, []);
 
   return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-        <Row>
-          <img
-            src={resume}
-            alt="home pic"
-            className="img-fluid"
-            style={{ maxHeight: "850px", marginTop: 70 }}
-          />
-        </Row>
-      </Container>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{meta.title} | Resume</title>
+        <meta name="description" content={meta.description} />
+      </Helmet>
+      <div>
+        <Container fluid className="resume-section">
+          <Particle />
+          <Row style={{ justifyContent: "center", position: "relative" }}>
+            <Button
+              variant="primary"
+              href={pdf}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV
+            </Button>
+          </Row>
+          <Row>
+            <img
+              src={resume}
+              alt="home pic"
+              className="img-fluid"
+              style={{ maxHeight: "850px", marginTop: 70 }}
+            />
+          </Row>
+        </Container>
+      </div>
+    </HelmetProvider>
   );
 }
 

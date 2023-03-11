75f6c34e3de2f6c31568a5c760288f9c70beb23e
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-11 20:08:15 +0530

    Added new resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import resume from "../../Assets/Sewmini_Sanjana_Resume.svg";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Sewmini_Sanjana.pdf";
+import pdf from "../../Assets/../Assets/Sewmini_Sanjana_Web.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink =
-  // "https://github.com/SANJANASOLANGA/WebPortfolio/raw/main/src/Assets/Sewmini_Sanjana.pdf";
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -37,16 +30,12 @@ function ResumeNew() {
         </Row>
         <Row>
           <img
-            src={homeLogo}
+          // Sewmini_Sanjana_Resume.svg
+            src={resume}
             alt="home pic"
             className="img-fluid"
             style={{ maxHeight: "450px" }}
           />
-        </Row>
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
         </Row>
       </Container>
     </div>

cb1362e7551f4496af9deba2c1e593f5487a2191
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2023-03-11 21:08:15 +0530

    Removed unwanted commented codes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,101 +1,10 @@
-/*import React, { useState, useEffect } from "react";
-import { Container,Row, Col, Alert } from "react-bootstrap";
-import "./style.css"
-function Contact() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
-  return (
-    <div>
-      <Container fluid className="resume-section">
-      <Row className="mb-5 mt-3 pt-md-3">
-          <Col lg="8">
-            <h1 className="display-4 mb-4">Contact Me</h1>
-            <hr className="t_border my-4 ml-0 text-left" />
-          </Col>
-        </Row>
-        <Row className="sec_sp">
-          <Col lg="5" className="mb-5">
-            <h3 className="color_sec py-4">Get in touch</h3>
-            <address>
-              <strong>Email:</strong>{" "}
-              <a href="mailto:sanjanasolangaarachchi@gmail.com">
-              sanjanasolangaarachchi@gmail.com
-              </a>
-              <br />
-              <br />
-                <p>
-                  <strong>Phone: +94719640127</strong> 
-                </p>
-            </address>
-          </Col>
-          <Col lg="7" className="d-flex align-items-center">
-            <form >
-              <Row>
-                <Col lg="6" className="form-group">
-                  <input
-                    className="form-control"
-                    id="name"
-                    name="name"
-                    placeholder="Name"
-                    // value={formData.name || ""}
-                    type="text"
-                    required
-                    // onChange={handleChange}
-                  />
-                </Col>
-                <Col lg="6" className="form-group">
-                  <input
-                    className="form-control rounded-0"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    type="email"
-                    // value={formData.email || ""}
-                    required
-                    // onChange={handleChange}
-                  />
-                </Col>
-              </Row>
-              <textarea
-                className="form-control rounded-0"
-                id="message"
-                name="message"
-                placeholder="Message"
-                rows="5"
-                // value={formData.message}
-                // onChange={handleChange}
-                required
-              ></textarea>
-              <br />
-              <Row>
-                <Col lg="12" className="form-group">
-                  <button className="btn ac_btn" type="submit">
-                    Send
-                  </button>
-                </Col>
-              </Row>
-            </form>
-          </Col>
-        </Row>        
-      </Container>
-    </div>
-  );
-}
-
-export default Contact;
-*/
-
 import React, { useState } from "react";
 import * as emailjs from "emailjs-com";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { meta } from "./content_option";
+import { meta } from "../content_option";
 import { Container, Row, Col, Alert } from "react-bootstrap";
-import { contactConfig } from "./content_option";
+import { contactConfig } from "../content_option";
 import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 

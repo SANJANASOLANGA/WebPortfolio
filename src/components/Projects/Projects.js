082ff3b1b4b16eb61663060deecaf5614d053927
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-28 01:46:24 +0530

    Updated descriptions of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -38,7 +38,7 @@ function Projects() {
                 imgPath={leo}
                 isBlog={false}
                 title="LeoWave"
-                description="LeoWave is the official mobile app developed for the Leo Club of District 306 C2, University of Sri Jayewardenepura, utilizing React Native and Firebase technologies. LeoWave features a board of officers directory for easy access to club leadership, provides information about Lions Club International, showcases ongoing and award-winning projects, and includes a contact us section for seamless communication."
+                description="LeoWave is the official mobile app developed for the Leo Club of District 306 C2, University of Sri Jayewardenepura, utilizing React Native, Javascript and Firebase technologies. LeoWave features a board of officers directory for easy access to club leadership, provides information about Lions Club International, showcases ongoing and award-winning projects, and includes a contact us section for seamless communication."
                 ghLink="https://github.com/SANJANASOLANGA/"
               />
             </Col>
@@ -47,7 +47,7 @@ function Projects() {
                 imgPath={nursery}
                 isBlog={false}
                 title="EduKids"
-                description="EduKids is a mobile app that gives nursery students the ability to learn and improve their knowledge simply by using the app. The app is built with React Native and Firebase. It has features which allows users to login, logout, and learn materials for alphabets, numbers, phrases, shapes, colors, as well as activities to improve knowledge."
+                description="EduKids is a mobile app that gives nursery students the ability to learn and improve their knowledge simply by using the app. The app is built with React Native, Javascript and Firebase. It has features which allows users to login, logout, and learn materials for alphabets, numbers, phrases, shapes, colors, as well as activities to improve knowledge."
                 ghLink="https://github.com/SANJANASOLANGA/NurseryApp"
               />
             </Col>
@@ -56,7 +56,7 @@ function Projects() {
                 imgPath={profile}
                 isBlog={false}
                 title="Profile for Faculty"
-                description="The profile app for faculty is designed to help faculty share news and maintain student profiles. It is built with React Native and Firebase, and has features which allow users to maintain their profiles in real time, read news, and faculty contact details. The app also provides guidelines for using the app, and for giving feedback."
+                description="The profile app for faculty is designed to help faculty share news and maintain student profiles. It is developed with React Native, Javascript and Firebase, and has features which allow users to maintain their profiles in real time, read news, and faculty contact details. The app also provides guidelines for using the app, and for giving feedback."
                 ghLink="https://github.com/SANJANASOLANGA/ProfileForFaculty"
               />
             </Col>
@@ -96,7 +96,7 @@ function Projects() {
                 imgPath={atm}
                 isBlog={false}
                 title="Automated Teller Machine"
-                description="Automated teller machines are a staple of modern banking. This project is a simple Automated teller machine built in Java using object-oriented programming. It has features which allow users for registration, login, logout, real-time cash deposit, withdrawal, balance inquiry, transaction history, as well as fund transfers."
+                description="Automated teller machines are a staple of modern banking. This project is a simple Automated teller machine developed in Java using object-oriented programming. It has features which allow users for registration, login, logout, real-time cash deposit, withdrawal, balance inquiry, transaction history, as well as fund transfers."
                 ghLink="https://github.com/SANJANASOLANGA/ATM_Project"
               />
             </Col>
@@ -116,7 +116,7 @@ function Projects() {
                 imgPath={aragi}
                 isBlog={false}
                 title="Aragi Hospital"
-                description="Aragi Hospital's website is full of useful content for anyone looking for a hospital. This website is built with HTML and CSS. They provide detailed information about their services, facilities, medical packages, and even career opportunities. You can also easily find their contact information and details about their international patients program."
+                description="Aragi Hospital's website is full of useful content for anyone looking for a hospital. This website is developed with HTML and CSS. This website provide detailed information about their services, facilities, medical packages, and even career opportunities. Users can easily find their contact information and details about their international patients program."
                 ghLink="https://github.com/SANJANASOLANGA/HospitalMgtSystem"
               />
             </Col>
@@ -125,7 +125,7 @@ function Projects() {
                 imgPath={personal}
                 isBlog={false}
                 title="Sanjana' s Personal Website"
-                description="Welcome to my personal website! This website exists as a means for you to find out more about me, see some of my previous projects, and get in touch if you'd like to collaborate on something. You can also find my resume here and links to my social media profiles. It's built with React and emailjs. I hope you enjoy exploring the site!"
+                description="Welcome to my personal website! This website exists as a means for you to find out more about me, see some of my previous projects, and get in touch if you'd like to collaborate on something. You can also find my resume here and links to my social media profiles. It is developed using React and emailjs technologies. I hope you enjoy exploring the site!"
                 ghLink="https://github.com/SANJANASOLANGA/WebPortfolio"
               />
             </Col>

6e47223f40f182c25d47edaccf7ff872e4bf87b9
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-28 01:49:41 +0530

    Updated about sub content

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Sewmini Sanjana </span>
             from <span className="purple"> Gampaha, Sri Lanka.</span>
-            <br />I am an undergraduate from the Department of ICT, University of Sri Jayewardenepura.
+            <br />I am a final-year IT undergraduate from the Department of ICT, University of Sri Jayewardenepura.
             <br />
             <br />
             Apart from coding, some other activities that I love to do are!
